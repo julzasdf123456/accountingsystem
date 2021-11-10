f749e0fac9a770f55d627f2da424c135b9334fa3
lentrix <hawkmanlentrix>
2021-11-11 07:05:36 +0800

    mirs-dao

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/dao/MirsDAO.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/dao/MirsDAO.java
@@ -6,9 +6,17 @@ import com.boheco1.dev.integratedaccountingsystem.objects.MIRSItem;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MirsDAO {
+
+    /**
+     * Creates a new MIRS record
+     * @param mirs The MIRS file to be created
+     * @throws Exception
+     */
     public static void create(MIRS mirs) throws Exception {
         PreparedStatement ps = DB.getConnection().prepareStatement(
                 "INSERT INTO MIRS (DateFiled, Purpose, Details, Status, RequisitionerID, UserID, CreatedAt, UpdateAt) " +
@@ -27,6 +35,11 @@ public class MirsDAO {
 
     }
 
+    /**
+     * Updates an existing MIRS record
+     * @param mirs The MIRS file to be updated
+     * @throws Exception
+     */
     public static void update(MIRS mirs) throws Exception {
         PreparedStatement ps = DB.getConnection().prepareStatement(
                 "UPDATE MIRS SET " +
@@ -43,6 +56,12 @@ public class MirsDAO {
         ps.close();
     }
 
+    /**
+     * Inserts a single MIRSItem in the MIRSItems table
+     * @param mirs The MIRS file for which an item will be added
+     * @param item The MIRSItem that will be added to the MIRS file
+     * @throws Exception
+     */
     public static void addMIRSItem(MIRS mirs, MIRSItem item) throws Exception {
         PreparedStatement ps = DB.getConnection().prepareStatement(
                 "INSERT INTO MIRSItems (MIRSID, StockID, Quantity, Price, Comments, CreatedAt, UpdatedAt) " +
@@ -59,6 +78,12 @@ public class MirsDAO {
         ps.close();
     }
 
+    /**
+     * Inserts a group of MIRSItems in to the MIRSItems table
+     * @param mirs The MIRS file for which the items will be added
+     * @param items The list of MIRSItem that will be added to the MIRS file
+     * @throws Exception
+     */
     public static void addMIRSItems(MIRS mirs, List<MIRSItem> items) throws Exception {
         PreparedStatement ps = DB.getConnection().prepareStatement(
                 "INSERT INTO MIRSItems (MIRSID, StockID, Quantity, Price, Comments, CreatedAt, UpdatedAt) " +
@@ -80,6 +105,11 @@ public class MirsDAO {
         ps.close();
     }
 
+    /**
+     * Updates an existing MIRSItem
+     * @param item The MIRSItem that will be updated
+     * @throws Exception
+     */
     public static void updateMIRSItem(MIRSItem item) throws Exception {
         PreparedStatement ps = DB.getConnection().prepareStatement(
                 "UPDATE MIRSItems SET StockID=?, Quantity=?, Price=?, Comments=?, UpdatedAt=NOW() " +
@@ -94,6 +124,11 @@ public class MirsDAO {
         ps.close();
     }
 
+    /**
+     * Removes a single MIRSItem
+     * @param item The MIRSItem that will be removed
+     * @throws Exception
+     */
     public static void removeItem(MIRSItem item) throws Exception {
         PreparedStatement ps = DB.getConnection().prepareStatement(
                 "DELETE FROM MIRSItems WHERE id=?");
@@ -104,6 +139,11 @@ public class MirsDAO {
         ps.close();
     }
 
+    /**
+     * Removes a group of records from the MIRSItem table
+     * @param items The List of MIRSItem that will removed
+     * @throws Exception
+     */
     public static void removeItems(List<MIRSItem> items) throws Exception {
         PreparedStatement ps = DB.getConnection().prepareStatement(
                 "DELETE FROM MIRSItems WHERE id=?");
@@ -116,5 +156,77 @@ public class MirsDAO {
         ps.executeBatch();
 
         ps.close();
+    }
+
+    /**
+     * Retrieves a single record from the MIRS table based on the id
+     * @param id The id of the MIRS record that will be retrieved
+     * @return MIRS object
+     * @throws Exception
+     */
+    public static MIRS getMIRS(int id) throws Exception {
+        PreparedStatement ps = DB.getConnection().prepareStatement(
+                "SELECT * FROM MIRS WHERE id=?");
+        ps.setInt(1, id);
+
+        ResultSet rs = ps.executeQuery();
+
+        if(rs.next()) {
+            MIRS mirs = new MIRS(
+                    rs.getInt("id"),
+                    rs.getDate("DateFiled").toLocalDate(),
+                    rs.getString("Purpose"),
+                    rs.getString("Details"),
+                    rs.getString("Status"),
+                    rs.getInt("RequesitionerID"),
+                    rs.getInt("UserID"),
+                    rs.getTimestamp("CreatedAt").toLocalDateTime(),
+                    rs.getTimestamp("UpdatedAt").toLocalDateTime()
+            );
+
+            rs.close();
+            ps.close();
+
+            return mirs;
+        }
+
+        rs.close();
+        ps.close();
+
+        return null;
+    }
+
+    /**
+     * Retrieves a group of MIRSItems that belong to a single MIRS record
+     * @param mirs the MIRS File from which the the MIRSItems belong
+     * @return List of MIRSItem
+     * @throws Exception
+     */
+    public static List<MIRSItem> getItems(MIRS mirs) throws Exception {
+        PreparedStatement ps = DB.getConnection().prepareStatement(
+                "SELECT * FROM MIRSItems WHERE MIRSID=?");
+        ps.setInt(1, mirs.getId());
+
+        ResultSet rs = ps.executeQuery();
+
+        ArrayList<MIRSItem> items = new ArrayList();
+
+        while(rs.next()) {
+            items.add(new MIRSItem(
+                    rs.getInt("id"),
+                    rs.getInt("MIRSID"),
+                    rs.getInt("StockID"),
+                    rs.getInt("Quantity"),
+                    rs.getDouble("Price"),
+                    rs.getString("Comments"),
+                    rs.getTimestamp("CreatedAt").toLocalDateTime(),
+                    rs.getTimestamp("UpdatedAt").toLocalDateTime()
+            ));
+        }
+
+        rs.close();
+        ps.close();
+
+        return items;
     }
 }
