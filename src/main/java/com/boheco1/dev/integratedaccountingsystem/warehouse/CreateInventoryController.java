113216459b67305f057a1b4f11ac8d1239d7a7c8
Daryl Valdez <xistenz03@gmail.com>
2021-11-23 16:56:19 +0800

    Added CreateInventoryController and layout.

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/warehouse/CreateInventoryController.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/warehouse/CreateInventoryController.java
@@ -2,6 +2,7 @@ package com.boheco1.dev.integratedaccountingsystem.warehouse;
 
 import com.boheco1.dev.integratedaccountingsystem.helpers.MenuControllerHandler;
 import com.boheco1.dev.integratedaccountingsystem.helpers.SubMenuHelper;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -10,6 +11,16 @@ import java.util.ResourceBundle;
 public class CreateInventoryController extends MenuControllerHandler implements Initializable, SubMenuHelper {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void clear(){
+
+    }
+
+    @FXML
+    public void updateStock(){
 
     }
 }
