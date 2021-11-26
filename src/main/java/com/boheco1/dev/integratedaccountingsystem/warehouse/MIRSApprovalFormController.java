359ad678cd25983886ab60fffa4f98449358afaf
reygodz <reyanthonygodmalin@gmail.com>
2021-11-27 00:49:37 +0800

    added details, accept and reject button

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/warehouse/MIRSApprovalFormController.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/warehouse/MIRSApprovalFormController.java
@@ -4,6 +4,8 @@ import com.boheco1.dev.integratedaccountingsystem.dao.MIRSSignatoryDAO;
 import com.boheco1.dev.integratedaccountingsystem.dao.MirsDAO;
 import com.boheco1.dev.integratedaccountingsystem.dao.StockDAO;
 import com.boheco1.dev.integratedaccountingsystem.dao.UserDAO;
+import com.boheco1.dev.integratedaccountingsystem.helpers.AlertDialogBuilder;
+import com.boheco1.dev.integratedaccountingsystem.helpers.Utility;
 import com.boheco1.dev.integratedaccountingsystem.objects.MIRS;
 import com.boheco1.dev.integratedaccountingsystem.objects.MIRSItem;
 import com.boheco1.dev.integratedaccountingsystem.objects.MIRSSignatory;
@@ -11,22 +13,33 @@ import com.boheco1.dev.integratedaccountingsystem.objects.Stock;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class MIRSPendingApprovalController implements Initializable {
-
+public class MIRSApprovalFormController implements Initializable {
+    @FXML
+    private StackPane stackPane;
 
     @FXML
-    private Label mirsNumber, date, purpose, requisitioner, dm, gm;
+    private HBox btnHolder;
+
+    @FXML
+    private TextArea details, purpose;
+
+    @FXML
+    private Label mirsNumber, date, requisitioner, dm, gm;
 
     @FXML
     private TableColumn<MIRSItem, String> codeCol, unitCol, particularsCol, remarksCol;
@@ -39,18 +52,18 @@ public class MIRSPendingApprovalController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("WarehouseDashboardController: "+WarehouseDashboardController.activeMIRS.getId());
         try {
-            MIRS mirs = MirsDAO.getMIRS(WarehouseDashboardController.activeMIRS.getId());
+            MIRS mirs = MirsDAO.getMIRS(Utility.getActiveMIRS().getId());
             List<MIRSItem> mirsItemList = MirsDAO.getItems(mirs);
             List<MIRSSignatory> mirsSignatoryList = MIRSSignatoryDAO.get(mirs);
 
             mirsNumber.setText(""+mirs.getId());
             date.setText(""+mirs.getDateFiled());
             purpose.setText(mirs.getPurpose());
+            details.setText(mirs.getDetails());
             requisitioner.setText(mirs.getRequisitioner().getFullName());
-            dm.setText(""+ UserDAO.get(mirsSignatoryList.get(1).getUserID()).getFullName());
-            gm.setText(""+ UserDAO.get(mirsSignatoryList.get(0).getUserID()).getFullName());
+            dm.setText(""+ UserDAO.get(mirsSignatoryList.get(0).getUserID()).getFullName());
+            gm.setText(""+ UserDAO.get(mirsSignatoryList.get(1).getUserID()).getFullName());
 
             ObservableList<MIRSItem> observableList = FXCollections.observableArrayList(mirsItemList);
 
@@ -70,6 +83,30 @@ public class MIRSPendingApprovalController implements Initializable {
             remarksCol.setCellValueFactory(new PropertyValueFactory<>("Remarks"));
             tableView.getItems().setAll(observableList);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void acceptBtn(ActionEvent event) {
+        try {
+            Utility.getActiveMIRS().setStatus("Releasing");
+            MirsDAO.update(Utility.getActiveMIRS());
+            AlertDialogBuilder.messgeDialog("System Message", "MIRS application approved and ready for releasing.", stackPane, AlertDialogBuilder.INFO_DIALOG);
+            btnHolder.setDisable(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void rejectBtn(ActionEvent event) {
+        try {
+            Utility.getActiveMIRS().setStatus("Rejected");
+            MirsDAO.update(Utility.getActiveMIRS());
+            AlertDialogBuilder.messgeDialog("System Message", "MIRS application rejected.", stackPane, AlertDialogBuilder.WARNING_DIALOG);
+            btnHolder.setDisable(true);
         } catch (Exception e) {
             e.printStackTrace();
         }
