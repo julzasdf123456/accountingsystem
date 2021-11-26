9d8c5c3c17ff49a5b6ec4ecc67bef032cdec204f
reygodz <reyanthonygodmalin@gmail.com>
2021-11-26 21:40:41 +0800

    add static variable active MIRS on Utility class

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/helpers/Utility.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/helpers/Utility.java
@@ -1,9 +1,19 @@
 package com.boheco1.dev.integratedaccountingsystem.helpers;
 
+import com.boheco1.dev.integratedaccountingsystem.objects.MIRS;
 import javafx.scene.layout.AnchorPane;
 
 public class Utility {
     private static AnchorPane contentPane;
+    private static MIRS activeMIRS;
+
+    public static MIRS getActiveMIRS() {
+        return activeMIRS;
+    }
+
+    public static void setActiveMIRS(MIRS activeMIRS) {
+        Utility.activeMIRS = activeMIRS;
+    }
 
     public static AnchorPane getContentPane() {
         return contentPane;
