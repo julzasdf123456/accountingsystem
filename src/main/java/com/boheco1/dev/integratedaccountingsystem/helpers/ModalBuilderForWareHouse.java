c9086b5f7e28f6c71f66c2c8b098abb88dac5615
zedlav <xistenz03@gmail.com>
2022-05-13 14:53:57 +0800

    Added PDF Builder helper class.
    Added new method for ModalBuilder to add action buttons.
    
    Added print employee MR in ViewMRsController.

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/helpers/ModalBuilderForWareHouse.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/helpers/ModalBuilderForWareHouse.java
@@ -1,6 +1,7 @@
 package com.boheco1.dev.integratedaccountingsystem.helpers;
 
 import com.boheco1.dev.integratedaccountingsystem.warehouse.CriticalStockController;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
 import javafx.fxml.FXMLLoader;
@@ -28,6 +29,40 @@ public class ModalBuilderForWareHouse {
             JFXDialogLayout dialogLayout = new JFXDialogLayout();
             dialogLayout.setBody(parent);
             JFXDialog dialog = new JFXDialog(stackPane, dialogLayout, JFXDialog.DialogTransition.BOTTOM);
+            dialog.setOnDialogClosed(e ->
+                    Utility.getContentPane().getChildren().setAll(ContentHandler.getNodeFromFxml(CriticalStockController.class, fxml)));
+            dialog.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void showModalFromXML(Class parentClass, String xml, StackPane stackPane, JFXButton action) {
+        try {
+            Parent parent = FXMLLoader.load(parentClass.getResource(xml));
+            JFXDialogLayout dialogLayout = new JFXDialogLayout();
+            dialogLayout.setBody(parent);
+            JFXDialog dialog = new JFXDialog(stackPane, dialogLayout, JFXDialog.DialogTransition.BOTTOM);
+            JFXButton cancel = new JFXButton("Cancel");
+            cancel.setOnAction(actionEvent -> dialog.close());
+            dialogLayout.setActions(action, cancel);
+            dialog.setOnDialogClosed(e ->
+                    Utility.getContentPane().getChildren().setAll(ContentHandler.getNodeFromFxml(CriticalStockController.class, "../warehouse_dashboard_controller.fxml")));
+            dialog.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void showModalFromXMLWithExitPath(Class parentClass, String xml, StackPane stackPane, String fxml, JFXButton action) {
+        try {
+            Parent parent = FXMLLoader.load(parentClass.getResource(xml));
+            JFXDialogLayout dialogLayout = new JFXDialogLayout();
+            dialogLayout.setBody(parent);
+            JFXDialog dialog = new JFXDialog(stackPane, dialogLayout, JFXDialog.DialogTransition.BOTTOM);
+            JFXButton cancel = new JFXButton("Cancel");
+            cancel.setOnAction(actionEvent -> dialog.close());
+            dialogLayout.setActions(action, cancel);
             dialog.setOnDialogClosed(e ->
                     Utility.getContentPane().getChildren().setAll(ContentHandler.getNodeFromFxml(CriticalStockController.class, fxml)));
             dialog.show();
