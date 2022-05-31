e09a9ec99914ef92c70616a41871a6fb4be28c88
reygodz <reyanthonygodmalin@gmail.com>
2022-06-01 00:49:22 +0800

    UI update and minor code correction

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/warehouse/ViewAllMIRSController.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/warehouse/ViewAllMIRSController.java
@@ -31,14 +31,11 @@ import org.kordamp.ikonli.javafx.FontIcon;
 import javax.swing.*;
 import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class ViewAllMIRSController extends MenuControllerHandler implements Initializable, SubMenuHelper {
 
-    @FXML TableView allMirsTable;
+    @FXML TableView mirsTable;
     @FXML private JFXTextField search_box;
     @FXML private JFXComboBox<Integer> month;
     @FXML private JFXTextField year;
@@ -49,7 +46,6 @@ public class ViewAllMIRSController extends MenuControllerHandler implements Init
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initializeMirsTable();
         populateMirsTable("");
-
         Calendar cal = Calendar.getInstance();
         year.setText(""+cal.get(Calendar.YEAR));
 
@@ -60,8 +56,7 @@ public class ViewAllMIRSController extends MenuControllerHandler implements Init
 
     private void initializeMirsTable() {
         try {
-
-            TableColumn<MIRS, String> mirsIdCol = new TableColumn<>("MIRS Number");
+            TableColumn<MIRS, String> mirsIdCol = new TableColumn<>("MIRS #");
             mirsIdCol.setPrefWidth(100);
             mirsIdCol.setMaxWidth(100);
             mirsIdCol.setMinWidth(100);
@@ -80,80 +75,18 @@ public class ViewAllMIRSController extends MenuControllerHandler implements Init
             mirsStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
 
             TableColumn<MIRS, String> purposeCol = new TableColumn<>("Purpose");
-            //purposeCol.setMinWidth(500);
             purposeCol.setCellValueFactory(new PropertyValueFactory<>("purpose"));
 
-            /*TableColumn<MIRS, MIRS> actionCol = new TableColumn<>("Action");
-            actionCol.setStyle("-fx-alignment: center;");
-            // ADD ACTION BUTTONS
-            actionCol.setCellValueFactory(mirsmirsCellDataFeatures -> new ReadOnlyObjectWrapper<>(mirsmirsCellDataFeatures.getValue()));
-            actionCol.setCellFactory(mirsmirsTableColumn -> new TableCell<>(){
-                FontIcon viewIcon =  new FontIcon("mdi2e-eye");
-                private final JFXButton viewButton = new JFXButton("", viewIcon);
-
-                FontIcon printIcon =  new FontIcon("mdi2p-printer");
-                private final JFXButton printButton = new JFXButton("", printIcon);
-
-                @Override
-                protected void updateItem(MIRS mirs, boolean b) {
-                    super.updateItem(mirs, b);
-
-                    if (mirs != null) {
-                        viewButton.setStyle("-fx-background-color: #2196f3;");
-                        viewIcon.setIconSize(13);
-                        viewIcon.setIconColor(Paint.valueOf(ColorPalette.WHITE));
-
-                        printButton.setStyle("-fx-background-color: #ff9800;");
-                        printIcon.setIconSize(13);
-                        printIcon.setIconColor(Paint.valueOf(ColorPalette.WHITE));
-
-                        viewButton.setOnAction(actionEvent -> {
-                            try {
-                                Utility.setActiveMIRS(mirs);
-                                ModalBuilderForWareHouse.showModalFromXMLWithExitPath(WarehouseDashboardController.class, "../warehouse_mirs_preview.fxml", Utility.getStackPane(), "../view_all_mirs_controller.fxml");
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        });
-
-                        printButton.setOnAction(actionEvent -> {
-                            try {
-                                new PrintMIRS(mirs);
-                            } catch (FileNotFoundException e) {
-                                e.printStackTrace();
-                            } catch (DocumentException e) {
-                                e.printStackTrace();
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        });
-
-
-                        HBox hBox = new HBox();
-                        hBox.setSpacing(2);
-                        hBox.getChildren().add(viewButton);
-                        hBox.getChildren().add(printButton);
-
-                        setGraphic(hBox);
-                    } else {
-                        setGraphic(null);
-                        return;
-                    }
-                }
-            });*/
-
-            allMirsTable.getColumns().removeAll();
-            allMirsTable.getColumns().add(mirsIdCol);
-            allMirsTable.getColumns().add(mirsDateFiled);
-            allMirsTable.getColumns().add(mirsStatus);
-            allMirsTable.getColumns().add(purposeCol);
-            //allMirsTable.getColumns().add(actionCol);
+            mirsTable.getColumns().removeAll();
+            mirsTable.getColumns().add(mirsIdCol);
+            mirsTable.getColumns().add(mirsDateFiled);
+            mirsTable.getColumns().add(mirsStatus);
+            mirsTable.getColumns().add(purposeCol);
         } catch (Exception e) {
             e.printStackTrace();
             AlertDialogBuilder.messgeDialog("Error", "Error initializing table: " + e.getMessage(), Utility.getStackPane(), AlertDialogBuilder.DANGER_DIALOG);
         }
     }
-
 
     @FXML
     private void searchMIRS(ActionEvent event) {
@@ -162,7 +95,7 @@ public class ViewAllMIRSController extends MenuControllerHandler implements Init
 
     @FXML
     private void viewMIRS(ActionEvent event) {
-        MIRS selected = (MIRS) allMirsTable.getSelectionModel().getSelectedItem();
+        MIRS selected = (MIRS) mirsTable.getSelectionModel().getSelectedItem();
         if (selected == null) {
             AlertDialogBuilder.messgeDialog("System Information", "Select item(s) before proceeding!", Utility.getStackPane(), AlertDialogBuilder.INFO_DIALOG);
             return;
@@ -179,8 +112,6 @@ public class ViewAllMIRSController extends MenuControllerHandler implements Init
     private void monthly_summary_btn(ActionEvent event) {
         try {
             ObservableList<MIRS> mirs = FXCollections.observableList(MirsDAO.getByMonthYear(year.getText() +"-"+ month.getSelectionModel().getSelectedItem()));
-           // allMirsTable.getItems().clear();
-            //allMirsTable.getItems().setAll(mirs);
             if (mirs.size() == 0) {
                 AlertDialogBuilder.messgeDialog("System Information", "No record on selected month and year.", Utility.getStackPane(), AlertDialogBuilder.INFO_DIALOG);
                 return;
@@ -213,13 +144,11 @@ public class ViewAllMIRSController extends MenuControllerHandler implements Init
         try {
             if(q.equals("")) {
                 ObservableList<MIRS> mirs = FXCollections.observableList(MirsDAO.getAllMIRS());
-                allMirsTable.getItems().clear();
-                allMirsTable.getItems().setAll(mirs);
+                mirsTable.getItems().setAll(mirs);
             }else {
-                allMirsTable.getItems().clear();
-                allMirsTable.getItems().add(MirsDAO.getMIRS(q));
+                ObservableList<MIRS> mirs = FXCollections.observableList(MirsDAO.searchMIRS(q));
+                mirsTable.getItems().setAll(mirs);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
             AlertDialogBuilder.messgeDialog("Error", "Error populating table: " + e.getMessage(), Utility.getStackPane(), AlertDialogBuilder.DANGER_DIALOG);
