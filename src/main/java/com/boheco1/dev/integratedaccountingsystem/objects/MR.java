cd8cb9ae64464bd1282df20b56dbcc03d7f5a82b
lentrix <hawkmanlentrix@gmail.com>
2022-06-02 05:33:45 +0800

    Object, DAO for MR and MRItem

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/objects/MR.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/objects/MR.java
@@ -1,49 +1,51 @@
 package com.boheco1.dev.integratedaccountingsystem.objects;
 
+import com.boheco1.dev.integratedaccountingsystem.dao.EmployeeDAO;
+import com.boheco1.dev.integratedaccountingsystem.helpers.DB;
+
 import java.time.LocalDate;
 
 public class MR {
     private String id;
     private String employeeId;
 
-    private String employeeFirstName;
-
-    private String employeeLastName;
+    private EmployeeInfo employeeInfo;
 
     private String warehousePersonnelId;
-    private String extItem;
-    private String stockId;
-    private int quantity;
-    private double price;
     private LocalDate dateOfMR;
     private String status; //active, lost, damaged, returned
     private LocalDate dateOfReturn;
 
-    private String remarks;
+    private String recommending;
+
+    private String approvedBy;
     public MR() {
     }
-    /**
-     * Used for creating MR with items not taken from the warehouse
-     */
-    public MR(String employeeId, String warehousePersonnelId, String extItem, int quantity, double price, LocalDate dateOfMR) {
-        this.employeeId = employeeId;
-        this.warehousePersonnelId = warehousePersonnelId;
-        this.extItem = extItem;
-        this.quantity = quantity;
-        this.price = price;
-        this.dateOfMR = dateOfMR;
-        this.status = "active";
+
+    public String getRecommending() {
+        return recommending;
     }
 
-    /**
-     * Used for creating MR with items from the warehouse
-     */
-    public MR(String employeeId, String warehousePersonnelId, String stockId, int quantity, LocalDate dateOfMR) {
+    public void setRecommending(String recommending) {
+        this.recommending = recommending;
+    }
+
+    public String getApprovedBy() {
+        return approvedBy;
+    }
+
+    public void setApprovedBy(String approvedBy) {
+        this.approvedBy = approvedBy;
+    }
+
+    public MR(String id, String employeeId, String warehousePersonnelId, LocalDate dateOfMR, String status, String recommending, String approvedBy) {
+        this.id = id;
         this.employeeId = employeeId;
         this.warehousePersonnelId = warehousePersonnelId;
-        this.stockId = stockId;
-        this.quantity = quantity;
         this.dateOfMR = dateOfMR;
+        this.status = status;
+        this.recommending = recommending;
+        this.approvedBy = approvedBy;
     }
 
     public String getId() {
@@ -70,38 +72,6 @@ public class MR {
         this.warehousePersonnelId = warehousePersonnelId;
     }
 
-    public String getExtItem() {
-        return extItem;
-    }
-
-    public void setExtItem(String extItem) {
-        this.extItem = extItem;
-    }
-
-    public String getStockId() {
-        return stockId;
-    }
-
-    public void setStockId(String stockId) {
-        this.stockId = stockId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public LocalDate getDateOfMR() {
         return dateOfMR;
     }
@@ -126,26 +96,14 @@ public class MR {
         this.dateOfReturn = dateOfReturn;
     }
 
-    public String getRemarks() {
-        return remarks;
-    }
-    public void setRemarks(String remarks) {
-        this.remarks = remarks;
-    }
-
-    public String getEmployeeFirstName() {
-        return employeeFirstName;
+    public EmployeeInfo getEmployeeInfo() throws Exception{
+        if(employeeInfo==null) {
+            this.employeeInfo = EmployeeDAO.getOne(employeeId, DB.getConnection());
+        }
+        return employeeInfo;
     }
 
-    public void setEmployeeFirstName(String employeeFirstName) {
-        this.employeeFirstName = employeeFirstName;
-    }
-
-    public String getEmployeeLastName() {
-        return employeeLastName;
-    }
-
-    public void setEmployeeLastName(String employeeLastName) {
-        this.employeeLastName = employeeLastName;
+    public void setEmployeeInfo(EmployeeInfo employeeInfo) {
+        this.employeeInfo = employeeInfo;
     }
 }
---
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/objects/MrItem.java
@@ -0,0 +1,73 @@
+package com.boheco1.dev.integratedaccountingsystem.objects;
+
+import com.boheco1.dev.integratedaccountingsystem.dao.StockDAO;
+import com.boheco1.dev.integratedaccountingsystem.helpers.DB;
+
+public class MrItem {
+    private String id;
+    private String stockID;
+    private int qty;
+    private String remarks;
+    private Stock stock;
+
+    private String mrNo;
+
+    public MrItem(String id, String mrNo, String stockID, int qty, String remarks) {
+        this.id = id;
+        this.mrNo = mrNo;
+        this.stockID = stockID;
+        this.qty = qty;
+        this.remarks = remarks;
+    }
+
+    public String getMrNo() {
+        return mrNo;
+    }
+
+    public void setMrNo(String mrNo) {
+        this.mrNo = mrNo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStockID() {
+        return stockID;
+    }
+
+    public void setStockID(String stockID) {
+        this.stockID = stockID;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public Stock getStock() throws Exception {
+        if(stock==null) {
+            stock = StockDAO.get(id);
+        }
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+}
