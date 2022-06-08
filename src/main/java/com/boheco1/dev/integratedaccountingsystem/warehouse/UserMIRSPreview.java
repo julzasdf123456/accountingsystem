b49748ac6a0f1422abd1f4131076916fe562bd9c
reygodz <reyanthonygodmalin@gmail.com>
2022-06-09 01:04:42 +0800

    user signatory approval dashboard

---
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/warehouse/UserMIRSPreview.java
@@ -0,0 +1,134 @@
+package com.boheco1.dev.integratedaccountingsystem.warehouse;
+
+import com.boheco1.dev.integratedaccountingsystem.dao.*;
+import com.boheco1.dev.integratedaccountingsystem.helpers.AlertDialogBuilder;
+import com.boheco1.dev.integratedaccountingsystem.helpers.DB;
+import com.boheco1.dev.integratedaccountingsystem.helpers.PrintPDF;
+import com.boheco1.dev.integratedaccountingsystem.helpers.Utility;
+import com.boheco1.dev.integratedaccountingsystem.objects.*;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Rectangle;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class UserMIRSPreview implements Initializable {
+
+    @FXML
+    private StackPane stackPane;
+    @FXML
+    private Label  details, date, mirsNumber, applicant, address, requisitioner, signatories, purpose;
+
+    @FXML
+    private JFXButton printRequest;
+    @FXML
+    private TableView requestedTable;
+
+
+    private MIRS mirs;
+    private List<Releasing> releasedIitems = null;
+    private List<ReleasedItemDetails> mirsItemList = null;
+    private List<MIRSSignatory> mirsSignatoryList;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        mirs = Utility.getActiveMIRS();
+
+        try {
+            mirs = MirsDAO.getMIRS(Utility.getActiveMIRS().getId());
+            mirsSignatoryList = MIRSSignatoryDAO.get(mirs);
+
+            String signatures = "";
+            for (MIRSSignatory sig:mirsSignatoryList) {
+                signatures+=sig.getStatus().toUpperCase()+" : "+UserDAO.get(sig.getUserID()).getFullName();
+                signatures+="\n";
+            }
+            signatories.setText(signatures);
+
+            mirsNumber.setText(""+mirs.getId());
+            date.setText(""+mirs.getDateFiled());
+            purpose.setText(mirs.getPurpose());
+            details.setText(mirs.getDetails());
+            address.setText(mirs.getAddress());
+            applicant.setText(mirs.getApplicant());
+            requisitioner.setText(mirs.getRequisitioner().getFullName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        populateRequestedMIRSInfo();
+    }
+
+    private void populateRequestedMIRSInfo(){
+        try {
+            mirsItemList = MirsDAO.getReleasedMIRSItems(mirs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ObservableList<ReleasedItemDetails> observableList = FXCollections.observableArrayList(mirsItemList);
+        TableColumn<ReleasedItemDetails, String> neaCodeCol = new TableColumn<>("NEA Code");
+        neaCodeCol.setStyle("-fx-alignment: center;");
+        neaCodeCol.setPrefWidth(150);
+        neaCodeCol.setMaxWidth(150);
+        neaCodeCol.setMinWidth(150);
+        neaCodeCol.setCellValueFactory(cellData -> {
+            try {
+                return new SimpleStringProperty(Objects.requireNonNull(StockDAO.get(cellData.getValue().getStockID())).getNeaCode());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
+
+        TableColumn<ReleasedItemDetails, String> descriptionCol = new TableColumn<>("Description");
+        descriptionCol.setCellValueFactory(cellData -> {
+            try {
+                return new SimpleStringProperty(Objects.requireNonNull(StockDAO.get(cellData.getValue().getStockID())).getDescription());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
+
+        TableColumn<ReleasedItemDetails, String> quantityCol = new TableColumn<>("Qty");
+        quantityCol.setStyle("-fx-alignment: center;");
+        quantityCol.setPrefWidth(50);
+        quantityCol.setMaxWidth(50);
+        quantityCol.setMinWidth(50);
+        quantityCol.setCellValueFactory(new PropertyValueFactory<>("Quantity"));
+
+
+        //requestedTable.getColumns().add(neaCodeCol);
+        requestedTable.getColumns().add(descriptionCol);
+        requestedTable.getColumns().add(quantityCol);
+        requestedTable.setPlaceholder(new Label("No item Added"));
+        requestedTable.getItems().setAll(observableList);
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/warehouse/ViewMIRSForApprovalController.java
@@ -0,0 +1,160 @@
+package com.boheco1.dev.integratedaccountingsystem.warehouse;
+
+import com.boheco1.dev.integratedaccountingsystem.HomeController;
+import com.boheco1.dev.integratedaccountingsystem.dao.EmployeeDAO;
+import com.boheco1.dev.integratedaccountingsystem.dao.MIRSSignatoryDAO;
+import com.boheco1.dev.integratedaccountingsystem.dao.MirsDAO;
+import com.boheco1.dev.integratedaccountingsystem.dao.UserDAO;
+import com.boheco1.dev.integratedaccountingsystem.helpers.*;
+import com.boheco1.dev.integratedaccountingsystem.objects.*;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+import java.net.URL;
+import java.util.Calendar;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ViewMIRSForApprovalController extends MenuControllerHandler implements Initializable, SubMenuHelper {
+
+    @FXML TableView mirsTable;
+    @FXML private JFXTextField remarks, workOrderNum;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeMirsTable();
+        ObservableList<MIRSSignatory> mirs = null;
+        try {
+            mirs = FXCollections.observableList(MIRSSignatoryDAO.getAllMIRS(ActiveUser.getUser()));
+            //mirs = FXCollections.observableList(MirsDAO.getAllMIRS());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mirsTable.getItems().setAll(mirs);
+
+        try {
+            if(EmployeeDAO.getOne(ActiveUser.getUser().getId(), DB.getConnection()).getDesignation().equals("Work Order Officer")){
+                workOrderNum.setDisable(false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void initializeMirsTable() {
+        try {
+            TableColumn<MIRS, String> mirsIdCol = new TableColumn<>("MIRS #");
+            mirsIdCol.setPrefWidth(100);
+            mirsIdCol.setMaxWidth(100);
+            mirsIdCol.setMinWidth(100);
+            mirsIdCol.setCellValueFactory(new PropertyValueFactory<>("mirsID"));
+
+            TableColumn<MIRS, String> mirsDateFiled = new TableColumn<>("Date Filed");
+            mirsDateFiled.setPrefWidth(150);
+            mirsDateFiled.setMaxWidth(150);
+            mirsDateFiled.setMinWidth(150);
+            mirsDateFiled.setCellValueFactory(new PropertyValueFactory<>("mirsdateFiled"));
+
+            TableColumn<MIRS, String> purposeCol = new TableColumn<>("Purpose");
+            purposeCol.setCellValueFactory(new PropertyValueFactory<>("purpose"));
+
+            mirsTable.getColumns().removeAll();
+            mirsTable.getColumns().add(mirsIdCol);
+            mirsTable.getColumns().add(mirsDateFiled);
+            mirsTable.getColumns().add(purposeCol);
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertDialogBuilder.messgeDialog("Error", "Error initializing table: " + e.getMessage(), Utility.getStackPane(), AlertDialogBuilder.DANGER_DIALOG);
+        }
+    }
+
+    @FXML
+    private void mirsTableClicked(MouseEvent event) throws Exception {
+        if(event.getClickCount() == 2){
+            MIRSSignatory temp = (MIRSSignatory) mirsTable.getSelectionModel().getSelectedItem();
+            MIRS selected = MirsDAO.getMIRS(temp.getMirsID());
+            if (selected == null) {
+                AlertDialogBuilder.messgeDialog("System Information", "Select item(s) before proceeding!", Utility.getStackPane(), AlertDialogBuilder.INFO_DIALOG);
+                return;
+            }
+            try {
+                Utility.setActiveMIRS(selected);
+                ModalBuilderForWareHouse.showModalFromXMLWithExitPath(WarehouseDashboardController.class, "../user_mirs_preview.fxml", Utility.getStackPane(), "../view_mirs_for_approval_controller.fxml");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    private void approvedMirs(ActionEvent event) {
+        action(Utility.APPROVED);
+    }
+
+    @FXML
+    private void rejectMirs(ActionEvent event) {
+        action(Utility.REJECTED);
+    }
+
+    private void action(String status){
+        JFXButton accept = new JFXButton("Proceed");
+        JFXDialog MIRSapprovalDialog = DialogBuilder.showConfirmDialog("MIRS Approval","Confirm action on MIRS application.", accept, Utility.getStackPane(), DialogBuilder.INFO_DIALOG);
+        accept.setTextFill(Paint.valueOf(ColorPalette.MAIN_COLOR));
+        accept.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent __) {
+                try {
+                    MIRSSignatory selected = (MIRSSignatory) mirsTable.getSelectionModel().getSelectedItem();
+                    if (selected == null) {
+                        AlertDialogBuilder.messgeDialog("System Information", "Select item(s) before proceeding!", Utility.getStackPane(), AlertDialogBuilder.INFO_DIALOG);
+                        return;
+                    }else{
+
+                        MIRS mirs = MirsDAO.getMIRS(selected.getMirsID());
+                        mirs.setWorkOrderNo(workOrderNum.getText());
+                        MirsDAO.update(mirs);
+
+                        MIRSSignatory temp = new MIRSSignatory();
+                        temp.setMirsID(selected.getMirsID());
+                        temp.setUserID(selected.getUserID());
+                        temp.setStatus(status);
+                        temp.setComments(remarks.getText());
+                        temp.setId(selected.getId());
+                        MIRSSignatoryDAO.update(temp);
+
+                        AlertDialogBuilder.messgeDialog("System Message", "MIRS application has been "+status,
+                                Utility.getStackPane(), AlertDialogBuilder.INFO_DIALOG);
+
+                        Utility.getContentPane().getChildren().setAll(ContentHandler.getNodeFromFxml(HomeController.class, "view_mirs_for_approval_controller.fxml"));
+
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    AlertDialogBuilder.messgeDialog("Error on ViewMIRSForApprovalController", e.getMessage(),
+                            Utility.getStackPane(), AlertDialogBuilder.DANGER_DIALOG);
+                }
+                MIRSapprovalDialog.close();
+            }
+        });
+    }
+}
