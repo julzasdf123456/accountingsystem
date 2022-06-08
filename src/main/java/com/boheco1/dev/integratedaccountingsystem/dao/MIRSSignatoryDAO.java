e3d9ef1073c5375f5b0679091c0e897f3885cd18
reygodz <reyanthonygodmalin@gmail.com>
2022-06-09 01:03:18 +0800

    added List<MIRSSignatory> getAllMIRS(User val) method

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/dao/MIRSSignatoryDAO.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/dao/MIRSSignatoryDAO.java
@@ -4,6 +4,7 @@ import com.boheco1.dev.integratedaccountingsystem.helpers.DB;
 import com.boheco1.dev.integratedaccountingsystem.helpers.Utility;
 import com.boheco1.dev.integratedaccountingsystem.objects.MIRS;
 import com.boheco1.dev.integratedaccountingsystem.objects.MIRSSignatory;
+import com.boheco1.dev.integratedaccountingsystem.objects.User;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -45,7 +46,35 @@ public class MIRSSignatoryDAO {
 
         ps.close();
     }
+    public static List<MIRSSignatory> getAllMIRS(User val) throws Exception {
+        PreparedStatement ps = DB.getConnection().prepareStatement(
+                "select MIRSSignatories.*, MIRS.DateFiled, MIRS.Purpose from MIRS LEFT JOIN MIRSSignatories on MIRS.id = MIRSSignatories.MIRSID WHERE MIRSSignatories.user_id = ? AND MIRSSignatories.Status = ? AND MIRS.Status = ? ORDER BY DateFiled DESC");
+        ps.setString(1, val.getId());
+        ps.setString(2, Utility.PENDING);
+        ps.setString(3, Utility.PENDING);
 
+        ResultSet rs = ps.executeQuery();
+
+        ArrayList<MIRSSignatory> mirsList = new ArrayList<>();
+
+        while(rs.next()) {
+            mirsList.add(new MIRSSignatory(
+                    rs.getString("id"),
+                    rs.getString("MIRSID"),
+                    rs.getString("user_id"),
+                    rs.getString("Status"),
+                    rs.getString("Comments"),
+                    rs.getTimestamp("CreatedAt").toLocalDateTime(),
+                    rs.getTimestamp("UpdatedAt").toLocalDateTime(),
+                    rs.getTimestamp("DateFiled").toLocalDateTime().toLocalDate(),
+                    rs.getString("Purpose")));
+        }
+
+        rs.close();
+        ps.close();
+
+        return mirsList;
+    }
     public static MIRSSignatory get(int id) throws Exception {
         PreparedStatement ps = DB.getConnection().prepareStatement(
                 "SELECT * FROM MIRSSignatories WHERE id=?");
@@ -75,8 +104,10 @@ public class MIRSSignatoryDAO {
 
     public static int getSignatoryCount(String  id) throws Exception {
         PreparedStatement ps = DB.getConnection().prepareStatement(
-                "SELECT COUNT(id) as TotalCount FROM MIRSSignatories WHERE MIRSID=? AND Status='pending'");
+                "SELECT COUNT(id) as TotalCount FROM MIRSSignatories WHERE MIRSID=? AND (Status=? OR Status=?)");
         ps.setString(1, id);
+        ps.setString(2, Utility.PENDING);
+        ps.setString(3, Utility.REJECTED);
 
         ResultSet rs = ps.executeQuery();
 
