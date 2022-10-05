ce91b13f0118d662eb9bf55a5f80bc415ca5cd57
Daryl B. Valdez <xistenz03@gmail.com>
2022-10-05 21:42:58 +0800

    Added secondary connection for other databases in same server and user credentials.

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/helpers/DB.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/helpers/DB.java
@@ -5,16 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-
+    //Main connection: Accounting database
     private static Connection connection;
+
+    //Other connection for other databases
+    private static Connection other_connection;
+
+    //Same server and user credentials
     public static String host="localhost";
-    public static String db_user = "sa";
-    public static String db_pass = "lntrx2022";
+    public static String db_user = "app_user";
+    public static String db_pass = "Boheco_2021";
+    public static String db_name = "Accounting";
+
+    //Default connection for Accounting DB
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         if(connection==null) {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String conString = "jdbc:sqlserver://"+host+";" +
-                    "Database=Accounting;" +
+                    "Database="+db_name+";" +
                     "user="+db_user+";" +
                     "password="+db_pass+";" +
                     "encrypt=false;" +
@@ -24,5 +32,22 @@ public class DB {
         }
 
         return connection;
+    }
+
+    //Connection for switching database and querying selected information such as in Tellering (Searching of consumer info in Billing db)
+    public static Connection getConnection(String db) throws SQLException, ClassNotFoundException {
+        if(other_connection==null) {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String conString = "jdbc:sqlserver://"+host+";" +
+                    "Database="+db+";" +
+                    "user="+db_user+";" +
+                    "password="+db_pass+";" +
+                    "encrypt=false;" +
+                    "trustServerCertificate=false;" +
+                    "loginTimeout=60";
+            other_connection = DriverManager.getConnection(conString);
+        }
+
+        return other_connection;
     }
 }
