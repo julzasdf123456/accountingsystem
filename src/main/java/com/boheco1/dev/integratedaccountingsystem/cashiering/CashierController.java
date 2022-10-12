3e2e06a7075f6b718d49bacf7f6c27b112ace641
reygodz <reyanthonygodmalin@gmail.com>
2022-10-12 23:43:59 +0800

    Add Cashier controller and UI

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/cashiering/CashierController.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/cashiering/CashierController.java
@@ -2,6 +2,10 @@ package com.boheco1.dev.integratedaccountingsystem.cashiering;
 
 import com.boheco1.dev.integratedaccountingsystem.helpers.MenuControllerHandler;
 import com.boheco1.dev.integratedaccountingsystem.helpers.SubMenuHelper;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.FlowPane;
 
@@ -10,14 +14,22 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class CashierController extends MenuControllerHandler implements Initializable, SubMenuHelper {
+
+    @FXML
+    private JFXTextField search_tf;
+
+    @FXML
+    private JFXButton search_btn;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
-    @Override
-    public void setSubMenus(FlowPane flowPane) {
-        flowPane.getChildren().removeAll();
-        flowPane.getChildren().setAll(new ArrayList<>());
+    @FXML
+    void search(ActionEvent event) {
+
     }
+
+
 }
