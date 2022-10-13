8ce08eae5485d7a2f24d62629d35a081c703876b
Rey <reyanthonygodmalin@gmail.com>
2022-10-13 11:51:28 +0800

    search consumer for cashiering

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/cashiering/CashierController.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/cashiering/CashierController.java
@@ -1,35 +1,63 @@
 package com.boheco1.dev.integratedaccountingsystem.cashiering;
 
+
 import com.boheco1.dev.integratedaccountingsystem.helpers.MenuControllerHandler;
-import com.boheco1.dev.integratedaccountingsystem.helpers.SubMenuHelper;
+import com.boheco1.dev.integratedaccountingsystem.helpers.ModalBuilderForWareHouse;
+import com.boheco1.dev.integratedaccountingsystem.helpers.ObjectTransaction;
+import com.boheco1.dev.integratedaccountingsystem.helpers.Utility;
+import com.boheco1.dev.integratedaccountingsystem.objects.ConsumerInfo;
+import com.boheco1.dev.integratedaccountingsystem.tellering.PowerBillsPaymentController;
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class CashierController extends MenuControllerHandler implements Initializable, SubMenuHelper {
-
-    @FXML
-    private JFXTextField search_tf;
+public class CashierController extends MenuControllerHandler implements Initializable, ObjectTransaction {
 
     @FXML
     private JFXButton search_btn;
 
+    @FXML
+    private JFXButton print_btn;
+
+    @FXML
+    private Label consumer, address, payable;
+
+    @FXML
+    private TableView paymentTable;
+
+
+    private ConsumerInfo consumerInfo = null;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        Utility.setParentController(this);
     }
 
     @FXML
-    void search(ActionEvent event) {
-
+    private void print(ActionEvent event) {
+        System.out.print("Print");
     }
 
+    @FXML
+    private void search(ActionEvent event) {
+        ModalBuilderForWareHouse.showModalFromXMLNoClose(CashierController.class, "../tellering/tellering_search_consumer.fxml", Utility.getStackPane());
+    }
+
+    @Override
+    public void receive(Object o) {
+        if (o instanceof ConsumerInfo) {
+            this.consumerInfo = (ConsumerInfo) o;
+            this.consumer.setText(consumerInfo.getConsumerName());
+            this.address.setText(consumerInfo.getConsumerAddress());
+        }
+    }
 
 }
