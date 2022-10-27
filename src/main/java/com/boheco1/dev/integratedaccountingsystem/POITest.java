c7e17f5c35eeb7137b5580ddd778b68ce6bb1c44
Daryl B. Valdez <xistenz03@gmail.com>
2022-10-27 23:39:38 +0800

    PrintOEBR template v1.

--- src/main/java/com/boheco1/dev/integratedaccountingsystem/POITest.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/POITest.java
@@ -1,8 +1,19 @@
 package com.boheco1.dev.integratedaccountingsystem;
 
 import com.boheco1.dev.integratedaccountingsystem.dao.*;
+import com.boheco1.dev.integratedaccountingsystem.helpers.CustomPrintHelper;
 import com.boheco1.dev.integratedaccountingsystem.helpers.Utility;
 import com.boheco1.dev.integratedaccountingsystem.objects.*;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -16,21 +27,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class POITest {
+public class POITest extends Application {
     public static void main(String[] args) {
-//        try {
-
-//            Notifications tochecker = new Notifications("details", "Utility.NOTIF_MIRS_APROVAL", "ActiveUser.getUser().getEmployeeID()", "checkedEmployeeInfo", "mirs");
-//            NotificationsDAO.create(tochecker);
-//            String ref = IRDao.getReceivingReference("1658746896033-OQ0RM8L0P29W9RV",2022,9);
-//            System.out.println(ref);
-//        }catch(Exception ex) {
-//            ex.printStackTrace();
-//        }
-//        executePOITest();
-        double money = 78945385.92;
-
-        System.out.println(Utility.doubleAmountToWords(money));
+        printOEBR();
     }
 
 
@@ -91,5 +90,205 @@ public class POITest {
             ex.printStackTrace();
         }
 
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        String meter_no = "Meter no:";
+        String type = "Type: RM";
+        String consumer = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy";
+        String billno = "Bill Number";
+        String address = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy";
+        String billmonth = "Bill Month";
+        String kwhUsed = "KWH Used";
+        String amount = "Bill Amount";
+        String vat = "Vat";
+        String dueDate = "Due Date";
+        String surcharge = "Surcharge";
+        String amountDue = "Amount Due";
+        String teller = "Teller";
+        String receivedDate = "Received date";
+
+        VBox container = new VBox();
+        container.setPadding(new Insets(25, 0, 0, 0));
+        container.setPrefWidth(612);
+        container.setPrefHeight(237.6);
+
+        HBox copyContainer = new HBox();
+
+        VBox left = new VBox();
+        VBox right = new VBox();
+
+        HBox.setHgrow(left, Priority.ALWAYS);
+        HBox.setHgrow(right, Priority.ALWAYS);
+
+        left.setMaxWidth(Double.MAX_VALUE);
+        right.setMaxWidth(Double.MAX_VALUE);
+
+        GridPane teller_grid = new GridPane();
+        GridPane consumer_grid = new GridPane();
+
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(33.3);
+
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(33.3);
+
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(33.4);
+
+        teller_grid.getColumnConstraints().addAll(column1,column2, column3);
+        consumer_grid.getColumnConstraints().addAll(column1,column2);
+
+        teller_grid.setMaxWidth(Double.MAX_VALUE);
+        consumer_grid.setMaxWidth(Double.MAX_VALUE);
+
+        Label spacer1 = new Label(" ");
+        Label spacer2 = new Label(" ");
+        Label spacer3 = new Label(" ");
+        Label spacer4 = new Label(" ");
+        Label spacer5 = new Label(" ");
+        Label spacer6 = new Label(" ");
+        Label spacer7 = new Label(" ");
+        Label spacer8 = new Label(" ");
+        Label spacer9 = new Label(" ");
+        Label spacer10 = new Label(" ");
+        Label spacer11 = new Label(" ");
+        Label spacer12 = new Label(" ");
+        Label spacer13 = new Label(" ");
+        Label spacer14 = new Label(" ");
+
+
+        Label surcharge_lbl = new Label("Surcharge");
+
+        Label tel_meter_lbl = new Label(meter_no);
+        Label tel_type_lbl = new Label(type);
+        Label tel_consumer_lbl = new Label(consumer);
+        Label tel_bill_lbl = new Label(billno);
+        Label tel_addr_lbl = new Label(address);
+        Label tel_billmonth_lbl = new Label(billmonth);
+        Label tel_kwh_lbl = new Label(kwhUsed);
+        Label tel_amount_lbl = new Label(amount);
+
+        Label tel_vat_lbl = new Label(vat);
+        Label tel_surcharge_lbl = new Label(surcharge);
+        Label tel_duedate_lbl = new Label(dueDate);
+        Label tel_amountDue_lbl = new Label(amountDue);
+        Label tel_teller_lbl = new Label(teller);
+        Label tel_receivedDate_lbl = new Label(receivedDate);
+
+        // page.add(Node, colIndex, rowIndex, colSpan, rowSpan):
+        teller_grid.add(tel_meter_lbl,0,0,2,1);
+        teller_grid.add(tel_type_lbl, 2, 0,1,1);
+        teller_grid.add(tel_consumer_lbl, 0, 1, 2,1);
+        teller_grid.add(tel_bill_lbl, 2, 1, 1,1);
+
+        teller_grid.add(tel_addr_lbl, 0, 2, 3,1);
+
+        teller_grid.add(spacer1, 1, 2, 3,1);
+        teller_grid.add(spacer2, 0, 3, 3,1);
+
+        teller_grid.add(tel_billmonth_lbl, 0, 4, 1,1);
+        teller_grid.add(tel_kwh_lbl, 1, 4, 1,1);
+        teller_grid.add(tel_amount_lbl, 2, 4, 1,1);
+
+        teller_grid.add(spacer3, 0, 5, 2,1);
+        teller_grid.add(tel_vat_lbl, 2, 5, 1,1);
+
+        teller_grid.add(spacer4, 0, 6, 3,1);
+
+        teller_grid.add(spacer5, 0, 7, 1,1);
+        teller_grid.add(surcharge_lbl, 1, 7, 1,1);
+        teller_grid.add(tel_surcharge_lbl, 2, 7, 1,1);
+
+        teller_grid.add(tel_duedate_lbl, 0, 8, 2,1);
+        teller_grid.add(tel_amountDue_lbl, 2, 8, 1,1);
+
+        teller_grid.add(spacer6, 0, 9, 3,1);
+        teller_grid.add(spacer7, 0, 10, 3,1);
+
+        teller_grid.add(tel_teller_lbl, 0, 11, 2,1);
+        teller_grid.add(tel_receivedDate_lbl, 2, 11, 1,1);
+
+        Label con_meter_lbl = new Label(meter_no);
+        Label con_type_lbl = new Label(type);
+        Label con_consumer_lbl = new Label(consumer);
+        Label con_bill_lbl = new Label(billno);
+        Label con_addr_lbl = new Label(address);
+
+        Label con_billmonth_lbl = new Label(billmonth);
+        Label con_kwh_lbl = new Label(kwhUsed);
+        Label con_amount_lbl = new Label(amount);
+
+        Label con_vat_lbl = new Label(vat);
+        Label con_surcharge_lbl = new Label(surcharge);
+        Label con_duedate_lbl = new Label(dueDate);
+        Label con_amountDue_lbl = new Label(amountDue);
+        Label con_teller_lbl = new Label(teller);
+        Label con_receivedDate_lbl = new Label(receivedDate);
+
+
+        consumer_grid.add(con_meter_lbl, 0, 0, 2 , 1);
+        consumer_grid.add(con_type_lbl, 2, 0, 1,1);
+        consumer_grid.add(con_consumer_lbl, 0, 1, 2,1);
+        consumer_grid.add(con_bill_lbl, 2, 1, 1,1);
+        consumer_grid.add(con_addr_lbl, 0, 2, 3,1);
+
+        consumer_grid.add(spacer8, 1, 2, 3,1);
+        consumer_grid.add(spacer9, 0, 3, 3 ,1);
+
+        consumer_grid.add(con_billmonth_lbl, 0, 4, 1,1);
+        consumer_grid.add(con_kwh_lbl, 1, 4, 1,1);
+        consumer_grid.add(con_amount_lbl, 2, 4, 1,1);
+
+        consumer_grid.add(spacer10, 0, 5, 2,1);
+        consumer_grid.add(con_vat_lbl, 2, 5, 1,1);
+
+        consumer_grid.add(spacer11, 0, 6, 3,1);
+
+        consumer_grid.add(spacer12, 0, 7, 1,1);
+        consumer_grid.add(surcharge_lbl, 1, 7, 1,1);
+        consumer_grid.add(con_surcharge_lbl, 2, 7, 1,1);
+
+        consumer_grid.add(con_duedate_lbl, 0, 8, 2,1);
+        consumer_grid.add(con_amountDue_lbl, 2, 8, 1,1);
+
+        consumer_grid.add(spacer13, 0, 9, 3,1);
+        consumer_grid.add(spacer14, 0, 10, 3,1);
+
+        consumer_grid.add(con_teller_lbl, 0, 11, 2,1);
+        consumer_grid.add(con_receivedDate_lbl, 2, 11, 1,1);
+
+        left.getChildren().add(teller_grid);
+        right.getChildren().add(consumer_grid);
+
+        //Add left and right containers to main node container
+        copyContainer.getChildren().addAll(left, right);
+        container.getChildren().add(copyContainer);
+
+        //Creating a scene object
+        Scene scene = new Scene(container);
+
+        //Setting title to the Stage
+        stage.setTitle("Grid Pane Example");
+
+        //Adding scene to the stage
+        stage.setScene(scene);
+
+        //Displaying the contents of the stage
+        stage.show();
+    }
+
+    public static void printOEBR(){
+        CustomPrintHelper print = new CustomPrintHelper("OEBR", 8.5, 3.3);
+        print.prepareOEBR(new PaidBill());
+        print.setOnFailed(e->{
+            System.out.println("Error!");
+        });
+        print.setOnSucceeded(e->{
+            System.out.println("Successful");
+        });
+        new Thread(print).start();
     }
 }
--- src/main/java/com/boheco1/dev/integratedaccountingsystem/helpers/CustomPrintHelper.java
+++ src/main/java/com/boheco1/dev/integratedaccountingsystem/helpers/CustomPrintHelper.java
@@ -1,10 +1,14 @@
 package com.boheco1.dev.integratedaccountingsystem.helpers;
 
+import com.boheco1.dev.integratedaccountingsystem.objects.PaidBill;
 import com.sun.javafx.print.PrintHelper;
 import com.sun.javafx.print.Units;
 import javafx.concurrent.Task;
+import javafx.geometry.Insets;
 import javafx.print.*;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 
 public class CustomPrintHelper extends Task {
 
@@ -16,6 +20,10 @@ public class CustomPrintHelper extends Task {
         this.paper = PrintHelper.createPaper(page, width, height, Units.INCH);
     }
 
+    public CustomPrintHelper(String page, double width, double height){
+        this.paper = PrintHelper.createPaper(page, width, height, Units.INCH);
+    }
+
     @Override
     protected Object call() throws Exception {
         print();
@@ -23,10 +31,10 @@ public class CustomPrintHelper extends Task {
     }
 
     public void print() throws Exception{
-        Printer printer = Printer.getDefaultPrinter();
+        //Printer printer = Printer.getDefaultPrinter();
 
         PrinterJob job = PrinterJob.createPrinterJob();
-
+        Printer printer = job.getPrinter();
         PageLayout layout = printer.createPageLayout(paper, PageOrientation.PORTRAIT, Printer.MarginType.EQUAL);
 
         JobSettings jobSettings = job.getJobSettings();
@@ -44,4 +52,180 @@ public class CustomPrintHelper extends Task {
         }
     }
 
+    public void prepareOEBR(PaidBill bill){
+
+        String meter_no = "Meter no:";
+        String type = "Type: RM";
+        String consumer = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy";
+        String billno = "Bill Number";
+        String address = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy";
+        String billmonth = "Bill Month";
+        String kwhUsed = "KWH Used";
+        String amount = "Bill Amount";
+        String vat = "Vat";
+        String dueDate = "Due Date";
+        String surcharge = "Surcharge";
+        String amountDue = "Amount Due";
+        String teller = "Teller";
+        String receivedDate = "Received date";
+
+        VBox container = new VBox();
+        container.setPadding(new Insets(25, 0, 0, 0));
+        container.setPrefWidth(612);
+        container.setPrefHeight(237.6);
+
+        HBox copyContainer = new HBox();
+
+        VBox left = new VBox();
+        VBox right = new VBox();
+
+        HBox.setHgrow(left, Priority.ALWAYS);
+        HBox.setHgrow(right, Priority.ALWAYS);
+
+        left.setMaxWidth(Double.MAX_VALUE);
+        right.setMaxWidth(Double.MAX_VALUE);
+
+        GridPane teller_grid = new GridPane();
+        GridPane consumer_grid = new GridPane();
+
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(33.3);
+
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(33.3);
+
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(33.4);
+
+        teller_grid.getColumnConstraints().addAll(column1,column2, column3);
+        consumer_grid.getColumnConstraints().addAll(column1,column2);
+
+        teller_grid.setMaxWidth(Double.MAX_VALUE);
+        consumer_grid.setMaxWidth(Double.MAX_VALUE);
+
+        Label spacer1 = new Label(" ");
+        Label spacer2 = new Label(" ");
+        Label spacer3 = new Label(" ");
+        Label spacer4 = new Label(" ");
+        Label spacer5 = new Label(" ");
+        Label spacer6 = new Label(" ");
+        Label spacer7 = new Label(" ");
+        Label spacer8 = new Label(" ");
+        Label spacer9 = new Label(" ");
+        Label spacer10 = new Label(" ");
+        Label spacer11 = new Label(" ");
+        Label spacer12 = new Label(" ");
+        Label spacer13 = new Label(" ");
+        Label spacer14 = new Label(" ");
+
+
+        Label surcharge_lbl = new Label("Surcharge");
+
+        Label tel_meter_lbl = new Label(meter_no);
+        Label tel_type_lbl = new Label(type);
+        Label tel_consumer_lbl = new Label(consumer);
+        Label tel_bill_lbl = new Label(billno);
+        Label tel_addr_lbl = new Label(address);
+        Label tel_billmonth_lbl = new Label(billmonth);
+        Label tel_kwh_lbl = new Label(kwhUsed);
+        Label tel_amount_lbl = new Label(amount);
+
+        Label tel_vat_lbl = new Label(vat);
+        Label tel_surcharge_lbl = new Label(surcharge);
+        Label tel_duedate_lbl = new Label(dueDate);
+        Label tel_amountDue_lbl = new Label(amountDue);
+        Label tel_teller_lbl = new Label(teller);
+        Label tel_receivedDate_lbl = new Label(receivedDate);
+
+        // page.add(Node, colIndex, rowIndex, colSpan, rowSpan):
+        teller_grid.add(tel_meter_lbl,0,0,2,1);
+        teller_grid.add(tel_type_lbl, 2, 0,1,1);
+        teller_grid.add(tel_consumer_lbl, 0, 1, 2,1);
+        teller_grid.add(tel_bill_lbl, 2, 1, 1,1);
+
+        teller_grid.add(tel_addr_lbl, 0, 2, 3,1);
+
+        teller_grid.add(spacer1, 1, 2, 3,1);
+        teller_grid.add(spacer2, 0, 3, 3,1);
+
+        teller_grid.add(tel_billmonth_lbl, 0, 4, 1,1);
+        teller_grid.add(tel_kwh_lbl, 1, 4, 1,1);
+        teller_grid.add(tel_amount_lbl, 2, 4, 1,1);
+
+        teller_grid.add(spacer3, 0, 5, 2,1);
+        teller_grid.add(tel_vat_lbl, 2, 5, 1,1);
+
+        teller_grid.add(spacer4, 0, 6, 3,1);
+
+        teller_grid.add(spacer5, 0, 7, 1,1);
+        teller_grid.add(surcharge_lbl, 1, 7, 1,1);
+        teller_grid.add(tel_surcharge_lbl, 2, 7, 1,1);
+
+        teller_grid.add(tel_duedate_lbl, 0, 8, 2,1);
+        teller_grid.add(tel_amountDue_lbl, 2, 8, 1,1);
+
+        teller_grid.add(spacer6, 0, 9, 3,1);
+        teller_grid.add(spacer7, 0, 10, 3,1);
+
+        teller_grid.add(tel_teller_lbl, 0, 11, 2,1);
+        teller_grid.add(tel_receivedDate_lbl, 2, 11, 1,1);
+
+        Label con_meter_lbl = new Label(meter_no);
+        Label con_type_lbl = new Label(type);
+        Label con_consumer_lbl = new Label(consumer);
+        Label con_bill_lbl = new Label(billno);
+        Label con_addr_lbl = new Label(address);
+
+        Label con_billmonth_lbl = new Label(billmonth);
+        Label con_kwh_lbl = new Label(kwhUsed);
+        Label con_amount_lbl = new Label(amount);
+
+        Label con_vat_lbl = new Label(vat);
+        Label con_surcharge_lbl = new Label(surcharge);
+        Label con_duedate_lbl = new Label(dueDate);
+        Label con_amountDue_lbl = new Label(amountDue);
+        Label con_teller_lbl = new Label(teller);
+        Label con_receivedDate_lbl = new Label(receivedDate);
+
+
+        consumer_grid.add(con_meter_lbl, 0, 0, 2 , 1);
+        consumer_grid.add(con_type_lbl, 2, 0, 1,1);
+        consumer_grid.add(con_consumer_lbl, 0, 1, 2,1);
+        consumer_grid.add(con_bill_lbl, 2, 1, 1,1);
+        consumer_grid.add(con_addr_lbl, 0, 2, 3,1);
+
+        consumer_grid.add(spacer8, 1, 2, 3,1);
+        consumer_grid.add(spacer9, 0, 3, 3 ,1);
+
+        consumer_grid.add(con_billmonth_lbl, 0, 4, 1,1);
+        consumer_grid.add(con_kwh_lbl, 1, 4, 1,1);
+        consumer_grid.add(con_amount_lbl, 2, 4, 1,1);
+
+        consumer_grid.add(spacer10, 0, 5, 2,1);
+        consumer_grid.add(con_vat_lbl, 2, 5, 1,1);
+
+        consumer_grid.add(spacer11, 0, 6, 3,1);
+
+        consumer_grid.add(spacer12, 0, 7, 1,1);
+        consumer_grid.add(surcharge_lbl, 1, 7, 1,1);
+        consumer_grid.add(con_surcharge_lbl, 2, 7, 1,1);
+
+        consumer_grid.add(con_duedate_lbl, 0, 8, 2,1);
+        consumer_grid.add(con_amountDue_lbl, 2, 8, 1,1);
+
+        consumer_grid.add(spacer13, 0, 9, 3,1);
+        consumer_grid.add(spacer14, 0, 10, 3,1);
+
+        consumer_grid.add(con_teller_lbl, 0, 11, 2,1);
+        consumer_grid.add(con_receivedDate_lbl, 2, 11, 1,1);
+
+        left.getChildren().add(teller_grid);
+        right.getChildren().add(consumer_grid);
+
+        //Add left and right containers to main node container
+        copyContainer.getChildren().addAll(left, right);
+        container.getChildren().add(copyContainer);
+        container.setStyle("-fx-font-size: 9px");
+        this.node = container;
+    }
 }
